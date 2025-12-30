403b8e9656506ee9db1f03674e0e6bc038cd5816
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-12-30 22:59:08 +0530

    routing basic setup

---
+++ message-brokers/rabbitmq/4-routing/producer.py
@@ -0,0 +1,9 @@
+import pika
+
+RABBITMQ_URL = "amqp://guest:guest@localhost:5672/"
+
+params = pika.URLParameters(url=RABBITMQ_URL)
+
+connection = pika.BlockingConnection(parameters=params)
+
+channel = connection.channel()
