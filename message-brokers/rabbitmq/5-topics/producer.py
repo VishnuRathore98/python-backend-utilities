a203d7af2d03b634755cabcf8a11f45f5da796f1
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-01-02 22:23:40 +0530

    topics stubbing

---
+++ message-brokers/rabbitmq/5-topics/producer.py
@@ -0,0 +1,10 @@
+import pika
+from pika.exchange_type import ExchangeType
+
+RABBITMQ_URL = "amqp://guest:guest@localhost:5672/"
+
+params = pika.URLParameters(url=RABBITMQ_URL)
+
+connection = pika.BlockingConnection(parameters=params)
+
+channel = connection.channel()
